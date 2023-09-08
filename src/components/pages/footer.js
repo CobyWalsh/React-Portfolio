9ed0208cefa59fddf05f7dd75a4014203aa0a90f
Coby Walsh <cobywalsh7@yahoo.com>
2023-09-08 07:48:46 -0600

    added footer.js to components

---
+++ src/components/pages/footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/footer.css';
+
+const styles = {
+    footerStyle: {
+      background: 'blue',
+    },
+    FooterStyle: {
+      fontSize: '60px',
+    },
+  };
+  
+  function footer() {
+    return (
+      <footer style={styles.footerStyle} className="footer">
+        <h1 style={styles.FooterStyle}>Contact Icons</h1>
+      </footer>
+    );
+  }
+  
+  export default footer;
+
+  
