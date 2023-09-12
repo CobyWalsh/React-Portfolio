950b8316d071acd23b12ecdb535176e430449f25
Coby Walsh <cobywalsh7@yahoo.com>
2023-09-12 08:51:31 -0600

    added content to contact file

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -15,6 +15,9 @@ function contact() {
   return (
     <header style={styles.contactStyle} id="contactMe" className="contact">
       <h1 style={styles.contactMeStyle}>Contact Me</h1>
+      <h2>Feel Free to Contact Me!</h2>
+      <h3>Email: cobywalsh89@gmail.com</h3>
+      <h3>Phone: (509)389-7222</h3>
     </header>
   );
 }
