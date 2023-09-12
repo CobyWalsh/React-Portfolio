64158e04a5504df06a02f5896f332ae30a51348b
Coby Walsh <cobywalsh7@yahoo.com>
2023-09-12 10:27:04 -0600

    added project and image to portfolio page

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -15,6 +15,15 @@ function portfolio() {
   return (
     <header style={styles.portfolioStyle} id="myPortfolio" className="portfolio">
       <h1 style={styles.myPortfolioStyle}>Portfolio</h1>
+      <div className="applications-other">
+                    <div>
+                        <figure className="figure">
+                            <figcaption>Group Project</figcaption>
+                            <a href="https://jenevaray.github.io/SuperGreen/">SuperGreen Webpage</a>
+                            <img src="00900003 copy.jpg" alt="Someone cutting vegitables"/>
+                        </figure>
+                    </div>
+                    </div>
     </header>
   );
 }
